af00d16e5d616a72963f4ce25a613b71f27cb7b8
unknown <arshadmiya34@gmail.com>
2024-10-03 17:11:28 +0530

    5 commit

--- gujarattoday/settings.py
+++ gujarattoday/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'django-insecure-+7@k4j6r_%50cvyaab*a_140gi=+by=0(8)&h69@yc4t7!frs$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['web-production-3308.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3308.up.railway.app/']
+SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
 # Application definition
 
