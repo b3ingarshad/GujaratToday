91485489652a92b13720c5b68974f3570f2684cd
unknown <arshadmiya34@gmail.com>
2024-10-09 19:50:41 +0530

    Server Start

--- gujarattoday/settings.py
+++ gujarattoday/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-+7@k4j6r_%50cvyaab*a_140gi=+by=0(8)&h69@yc4t7!frs$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-3308.up.railway.app', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
